974ea54f1029be6aedf0c15d38b9ec0ba055e1f7
GabiIva <gabigreen56@gmail.com>
2026-02-28 01:46:41 +0200

    Simplify Message interface and delivery methods
    Refactored Message interface and its implementations for brevity.

--- Java/MessengerApp.java
+++ Java/MessengerApp.java
@@ -1,24 +1,15 @@
-interface Message {
-    void deliver();
-}
+interface Message { void deliver(); }
 
 class SlackMessage implements Message {
-    @Override
-    public void deliver() {
-        System.out.println("Изпращане на съобщение в Slack...");
-    }
+    public void deliver() { System.out.println("Изпращане в Slack..."); }
 }
 
 class DiscordMessage implements Message {
-    @Override
-    public void deliver() {
-        System.out.println("Изпращане на съобщение в Discord...");
-    }
+    public void deliver() { System.out.println("Изпращане в Discord..."); }
 }
 
 abstract class Messenger {
-   public abstract Message createMessage();
-
+    public abstract Message createMessage(); 
     public void send() {
         Message msg = createMessage();
         msg.deliver();
@@ -26,25 +17,16 @@ abstract class Messenger {
 }
 
 class SlackMessenger extends Messenger {
-    @Override
-    public Message createMessage() {
-        return new SlackMessage();
-    }
+    public Message createMessage() { return new SlackMessage(); }
 }
 
 class DiscordMessenger extends Messenger {
-    @Override
-    public Message createMessage() {
-        return new DiscordMessage();
-    }
+    public Message createMessage() { return new DiscordMessage(); }
 }
 
 public class MessengerApp {
     public static void main(String[] args) {
-        Messenger myApp = new SlackMessenger();
-        myApp.send(); 
-        
-        Messenger myOtherApp = new DiscordMessenger();
-        myOtherApp.send(); 
+        Messenger app = new SlackMessenger();
+        app.send();
     }
 }
